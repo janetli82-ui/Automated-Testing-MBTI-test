982c85bbe541faec7f2c5f5693a7b638f4e80e20
wineynia <gabiybiy@gmail.com>
2026-08-24 12:34:48 +0200

    Formatting result import link

--- __tests__/Result.test.tsx
+++ __tests__/Result.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen} from '@testing-library/react'
-import Result from '../components/Result'
+import Result from '@/components/Result'
 
 const mockResultData = {
         title:"Strategic Architect",
