14b389fccc885324b2e8282791f9cb6a2d24c701
wineynia <gabiybiy@gmail.com>
2026-08-24 13:20:10 +0200

    Add ProgressBar to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 
 import { useState } from "react";
 import QA from "@/components/QA";
+import { mbtiQuestions } from "@/data/data";
+import ProgressBar from "@/components/ProgressBar";
 
 
 export default function Home() {
   const [currentNumber, setCurrentNumber] = useState(0)
+  const [answers, setAnswers] = useState<string[]>([])
+  
+  const handleSelectClick = (answerType: string) => {
+    const updatedAnswers = [...answers, answerType]
+    setAnswers(updatedAnswers)
 
-  const handleSelectClick = () => {
     setCurrentNumber(prev => prev + 1)
   }
 
@@ -15,7 +21,13 @@ export default function Home() {
     setCurrentNumber(prev => prev - 1)
   }
   return (
-    <main>
+    <main className="mx-auto flex w-full max-w-3xl flex-1 flex-col px-6 py-10">
+      <div className="mb-8">
+        <ProgressBar
+          currentNumber={currentNumber}
+          totalQuestions={mbtiQuestions.length}
+        />
+      </div>
       <QA currentNumber={currentNumber} selectAnswer={handleSelectClick}  onPrev={buttonClick}/>
     </main>
   );
